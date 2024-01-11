a4f7cb8e24a8ab6c250e4a0ce097eba9853249f7
aycrown1 <ayoadeadedamola198@gmail.com>
2024-01-11 22:02:16 +0000

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module contains a type-annotated function named concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    function concat takes a string str1 and a string str2
+            as arguments and returns a concatenated string
+   """
+    return str1 + str2
