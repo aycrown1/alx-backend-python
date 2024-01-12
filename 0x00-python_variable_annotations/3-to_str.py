9d727e6ae83392e69041a2d2c272f037b8b29701
aycrown1 <ayoadeadedamola198@gmail.com>
2024-01-12 00:05:09 +0000

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module contains a type-annotated function named to_str
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    function takes a float n as argument and returns the string representation of the float.
+   """
+    return str(n)
