e7f6f143720f0c124f73bdea7567137caaf7748a
aycrown1 <ayoadeadedamola198@gmail.com>
2024-02-10 02:24:40 +0000

    Parameterize

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -55,3 +55,16 @@ class TestGithubOrgClient(unittest.TestCase):
                              ["repo1", "repo2"])
             mock_get_json.assert_called_once()
             mock_public_repos_url.assert_called_once()
+
+    @parameterized.expand(
+        [
+            ({"license": {"key": "my_license"}}, "my_license", True),
+            ({"license": {"key": "other_license"}}, "my_license", False),
+        ]
+    )
+    def test_has_license(self, repo, license_key, expected_result) -> None:
+        """unit-test GithubOrgClient.has_license"""
+        github_org_client = GithubOrgClient("google")
+        self.assertEqual(
+            github_org_client.has_license(repo, license_key), expected_result
+        )
