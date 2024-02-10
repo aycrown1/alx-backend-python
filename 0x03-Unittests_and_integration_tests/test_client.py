1b82c071056678bf91688d749bb61ea0e7896bfd
aycrown1 <ayoadeadedamola198@gmail.com>
2024-02-10 02:40:01 +0000

    Integration test and Integration test: fixtures

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 module to the test client.GithubOrgClient class
 """
 from unittest.mock import patch, Mock, PropertyMock
-from parameterized import parameterized
+from parameterized import parameterized, parameterized_class
 from client import GithubOrgClient
 import unittest
 
@@ -68,3 +68,58 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(
             github_org_client.has_license(repo, license_key), expected_result
         )
+
+
+@parameterized_class(('org_payload', 'repos_payload',
+                      'expected_repos', 'apache2_repos'), TEST_PAYLOAD)
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """Set up the tests"""
+        cls.get_patcher = patch("requests.get")
+        cls.mock_get = cls.get_patcher.start()
+
+        def side_effect(url):
+            """
+             to make sure the mock of requests.get(url).json() returns
+            the correct fixtures for the various values of url
+            that you anticipate to receive.
+            """
+            class MockResponse:
+                def __init__(self, json_data):
+                    """ initializes the json_data
+                    """
+                    self.json_data = json_data
+
+                def json(self):
+                    """
+                    returns the correct fixtures for the various values of url
+                    that you anticipate to receive.
+                    """
+                    return self.json_data
+            if url.endswith("/orgs/google"):
+                return MockResponse(cls.org_payload)
+            elif url.endswith("/orgs/google/repos"):
+                return MockResponse(cls.repos_payload)
+            else:
+                return None
+
+        cls.mock_get.side_effect = side_effect
+
+    @classmethod
+    def tearDownClass(cls):
+        """Tear down the tests"""
+        cls.get_patcher.stop()
+
+    def test_public_repos(self):
+        """method to test GithubOrgClient.public_repos."""
+        github_org_client = GithubOrgClient("google")
+        self.assertEqual(github_org_client.public_repos(), self.expected_repos)
+
+    def test_public_repos_with_license(self):
+        """test the public_repos with the argument license="apache-2.0"
+        and make sure the result matches the expected
+        value from the fixtures."""
+        github_org_client = GithubOrgClient("google")
+        self.assertEqual(github_org_client.public_repos(license="apache-2.0"),
+                         self.apache2_repos)
