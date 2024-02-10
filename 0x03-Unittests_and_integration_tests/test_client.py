9b912b487da17b9b894a1ff2a59d0b60da928566
aycrown1 <ayoadeadedamola198@gmail.com>
2024-02-10 02:13:11 +0000

    Mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """
 module to the test client.GithubOrgClient class
 """
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 import unittest
@@ -26,3 +26,13 @@ class TestGithubOrgClient(unittest.TestCase):
         github_org_client = GithubOrgClient(org_name)
         self.assertEqual(github_org_client.org, {"payload": True})
         mock_get.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+
+    @patch("client.GithubOrgClient.org", new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org) -> None:
+        """
+        unit-test GithubOrgClient._public_repos_url"""
+        payload = {"repos_url": "https://api.github.com/orgs/google/repos"}
+        mock_org.return_value = payload
+        github_org_client = GithubOrgClient("google")
+        self.assertEqual(github_org_client._public_repos_url,
+                         payload["repos_url"])
